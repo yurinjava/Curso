d53038ac1181573b995aac7065fbba899e705a3c
yurokkj <yurisouzact@gmail.com>
2022-06-27 20:23:06 -0300

    --

--- src/entities/OutsourcedEmployee.java
+++ src/entities/OutsourcedEmployee.java
@@ -5,20 +5,16 @@ public class OutsourcedEmployee extends Employee {
 
 	public OutsourcedEmployee() {
 	}
-	
 	public OutsourcedEmployee(String name, Integer hour, Double valuePerHour, Double additionalCharge) {
 		super(name, hour, valuePerHour);
 		this.additionalCharge = additionalCharge;
 	}
-
 	public Double getAdditionalCharge() {
 		return additionalCharge;
 	}
-
 	public void setAdditionalCharge(Double additionalCharge) {
 		this.additionalCharge = additionalCharge;
 	}
-	
 	@Override
 	public double payment() {
 		return super.payment()+additionalCharge*1.1;
